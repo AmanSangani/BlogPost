5b6fa07d90538c7b60a6e0540d52ce32cd020249
AmanSangani <sanganiaman1310@gmail.com>
2024-06-15 11:17:06 +0530

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice'
+
 import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <>
-      <h1>Blog</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) {
+    return (
+      <>
+        <div className='min-h-screen flex flex-wrap content-between bg-gray-700'>
+          <div className='w-full block'>
+            <h1>Loading</h1>
+          </div>
+        </div>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <div className='min-h-screen flex flex-wrap content-between bg-gray-700 rounded-lg'>
+          <div className='w-full block'>
+            <Header />
+            <main>
+              {/* <Outlet /> */}
+            </main>
+            <Footer />
+          </div>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App
