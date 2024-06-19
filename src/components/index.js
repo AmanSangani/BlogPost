1f511a4473250b6ce4939cc91ac18c398431224b
AmanSangani <sanganiaman1310@gmail.com>
2024-06-19 16:45:36 +0530

    login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer";
 import Container from "./Container";
 import Logo from "./Logo";
 import LogoutBtn from "./LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
