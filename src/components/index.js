54ba3533d8c72293ddcc4912ce68e6939d1680cb
AmanSangani <sanganiaman1310@gmail.com>
2024-06-25 10:56:03 +0530

    postForm complete

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,16 @@ import LogoutBtn from "./LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
 import RTE from "./RTE";
+import Select from "./Select";
 
-
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, RTE };
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    RTE,
+    Select,
+};
